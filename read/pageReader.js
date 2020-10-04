3a655cb0db1512e32010e9c1df02ada330fff4fa
croftr <rob@robincroft.com>
2020-10-04 12:51:24 +0100

    crawl from base url

--- read/pageReader.js
+++ read/pageReader.js
@@ -3,10 +3,23 @@ const readerResource = require("../rest/readerResource")
 
 module.exports = {
     readDom: async () => {
-        const response = await readerResource.getArticle();
+
+        const response = await readerResource.getArticle('https://www.theguardian.com');
         const dom = new jsdom.JSDOM(response);
-        const title = dom.window.document.title;                
-        console.log('got title', title);        
+        const links = dom.window.document.querySelectorAll('a');
+        console.log('got links', links);
+
+        for (let link of links) {
+            console.log('link: ', link.href);
+            if (link.href.startsWith('http')) {
+                const response = await readerResource.getArticle(link);
+                const dom = new jsdom.JSDOM(response);
+                const title = dom.window.document.title;
+                console.log('title ', title);
+            }
+        };
+
+
     }
 }
 
--- rest/readerResource.js
+++ rest/readerResource.js
@@ -1,8 +1,10 @@
 const fetch = require('node-fetch');
 
 module.exports = {
-    getArticle: async () => {
-        const response = await fetch('https://www.theguardian.com/us-news/2020/oct/04/i-feel-much-better-trump-releases-first-video-message-from-hospital-room');                
+    getArticle: async (url) => {
+        console.log('get articles from ', url);
+        
+        const response = await fetch(url);                
         const body = await response.text();            
         return body;    
     }
