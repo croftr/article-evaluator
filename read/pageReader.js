27f2e91ea243f3d471ef04966cbdaf2e003bf8e8
croftr <rob@robincroft.com>
2020-10-04 13:52:32 +0100

    can add tags to specifiy articles you are interested in

--- read/pageReader.js
+++ read/pageReader.js
@@ -2,23 +2,39 @@ const jsdom = require("jsdom");
 const readerResource = require("../rest/readerResource")
 const sentimentReader = require("../analyze/sentimentReader");
 
+const tags = ['trump']
+
 module.exports = {
     readDom: async () => {
 
-        const response = await readerResource.getArticle('https://www.theguardian.com');
+        const response = await readerResource.getArticle('https://www.theguardian.com/world');
         const dom = new jsdom.JSDOM(response);
         const links = dom.window.document.querySelectorAll('a');
-    
+
         for (let link of links) {
-            // console.log('link: ', link.href);
+
             if (link.href.startsWith('http')) {
                 const response = await readerResource.getArticle(link);
                 const dom = new jsdom.JSDOM(response);
                 const title = dom.window.document.title;
-                console.log('title ', title);
-                const sentiment = sentimentReader.getSentiment(title);
-                console.log('sentiment ', sentiment);
-                
+
+                const titleLower = title.toLowerCase();
+
+                let match = false;
+
+                for (let tag of tags) {
+                    if (titleLower.includes(tag)) {
+                        match = true;
+                        break;
+                    }
+                }
+
+                if (match) {
+                    console.log(title);
+                    const sentiment = sentimentReader.getSentiment(title);
+                    console.log(sentiment);
+                }
+
                 // console.log('Sentiment ', sentiment > 0 ? 'POSITIVE' : sentiment === 0 ? 'NEAUTRAL' : 'NEGATIVE');
             }
         };
