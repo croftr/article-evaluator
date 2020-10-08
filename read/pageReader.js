7f7f1f3cccc640d90f6e623b35b28b12542158c6
barooahn <barooahn@gmail.com>
2020-10-08 13:20:12 +0100

    put results into files

--- read/pageReader.js
+++ read/pageReader.js
@@ -1,52 +1,63 @@
 const jsdom = require("jsdom");
-const readerResource = require("../rest/readerResource")
+const readerResource = require("../rest/readerResource");
 const sentimentReader = require("../analyze/sentimentReader");
 
-const tags = ['trump']
+const tags = ["trump"];
 
 module.exports = {
-    readDom: async () => {
+  readDom: async () => {
+    let finalScore = 0;
 
-        let finalScore =0;
+    const response = await readerResource.getArticle(
+      "https://www.theguardian.com/world"
+    );
+    const dom = new jsdom.JSDOM(response);
+    const links = dom.window.document.querySelectorAll("a");
+    let results = [];
 
-        const response = await readerResource.getArticle('https://www.theguardian.com/world');
+    for (let link of links) {
+      if (link.href.startsWith("http")) {
+        const response = await readerResource.getArticle(link);
         const dom = new jsdom.JSDOM(response);
-        const links = dom.window.document.querySelectorAll('a');
+        const title = dom.window.document.title;
 
-        for (let link of links) {
+        const titleLower = title.toLowerCase();
 
-            if (link.href.startsWith('http')) {
-                const response = await readerResource.getArticle(link);
-                const dom = new jsdom.JSDOM(response);
-                const title = dom.window.document.title;
+        let match = false;
 
-                const titleLower = title.toLowerCase();
-                    
-                let match = false;
+        for (let tag of tags) {
+          if (titleLower.includes(tag)) {
+            match = true;
+            break;
+          }
+        }
 
-                for (let tag of tags) {
-                    if (titleLower.includes(tag)) {
-                        match = true;
-                        break;
-                    }
-                }
+        if (match) {
+          console.log(title);
+          const sentimentScore = sentimentReader.getSentiment(title);
+          const sentimentResult =
+            sentimentScore > 0
+              ? "POSITIVE"
+              : sentimentScore === 0
+              ? "NEUTRAL"
+              : "NEGATIVE";
+          finalScore = finalScore + sentimentScore;
+          console.log(sentimentResult);
+          console.log("Accumulated score: ", finalScore);
 
-                if (match) {
-                    console.log(title);
-                    const sentimentScore = sentimentReader.getSentiment(title);                                    
-                    const sentimentResult = sentimentScore> 0 ? 'POSITIVE' : sentimentScore === 0 ? 'NEAUTRAL' : 'NEGATIVE'
-                    finalScore = finalScore + sentimentScore;
-                    console.log(sentimentResult);
-                    console.log('Accumulated score: ', finalScore);
-                    
-                }
-                
-            }            
-        };
-
-        console.log(`Overall score for ${tags.join(' ')} is ${finalScore}`);
-        console.log(`Overall sentiment for ${tags.join(' ')} is ${finalScore> 0 ? 'POSITIVE' : finalScore === 0 ? 'NEAUTRAL' : 'NEGATIVE'}`);
-
+          results.push({ text: title, sentiment: sentimentResult });
+        }
+      }
     }
-}
+    const filesWritten = readerResource.writeToFile(results);
 
+    console.log("files written: ", filesWritten);
+
+    console.log(`Overall score for ${tags.join(" ")} is ${finalScore}`);
+    console.log(
+      `Overall sentiment for ${tags.join(" ")} is ${
+        finalScore > 0 ? "POSITIVE" : finalScore === 0 ? "NEUTRAL" : "NEGATIVE"
+      }`
+    );
+  },
+};
--- rest/readerResource.js
+++ rest/readerResource.js
@@ -1,9 +1,40 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+const fs = require("fs");
 
 module.exports = {
-    getArticle: async (url) => {                
-        const response = await fetch(url);                
-        const body = await response.text();            
-        return body;    
+  getArticle: async (url) => {
+    const response = await fetch(url);
+    const body = await response.text();
+    return body;
+  },
+
+  writeToFile: async (results) => {
+    //take results and loop through
+    //if positive put write to positive folder, negative to negative and neutral ignore
+    //each file should only be text
+    //console.log("results are:", results);
+    let totalFiles = 0;
+    for (const [index, result] of results.entries()) {
+      console.log("result", result);
+      if (result.sentiment === "POSITIVE") {
+        const writeStream = await fs.createWriteStream(
+          `./results/pos/pos_${index.toString()}.txt`
+        );
+        writeStream.write(result.text);
+        writeStream.end();
+        totalFiles++;
+      } else if (result.sentiment === "NEGATIVE") {
+        const writeStream = await fs.createWriteStream(
+          `./results/neg/neg_${index.toString()}.txt`
+        );
+        writeStream.write(result.text);
+        writeStream.end();
+        totalFiles++;
+      } else {
+        continue;
+      }
     }
-}
+
+    return `files written = ${totalFiles}`;
+  },
+};
