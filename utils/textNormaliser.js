a1bace690a5631a86143d8b43c0bc19a782ed43c
croftr <rob@robincroft.com>
2020-10-25 18:11:09 +0000

    change regex

---
+++ utils/textNormaliser.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    normalise: (text) => {             
+        const specialCharsRemoved = text.replace(/[^a-zA-Z0-9]/g,' ');
+        return specialCharsRemoved.toLowerCase()
+    }
+}
+
