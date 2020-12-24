908c5ec318bbf2407dedbcbc6c53d61666d1f599
croftr <rob@robincroft.com>
2020-12-24 18:30:35 +0000

    get pub date

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -1,50 +1,53 @@
 var logger = require('../logger');
 var params = require('../params');
 
-const findArticleDate = (document) => {             
+const findArticleDate = (document) => {
 
     let articleDate;
 
     const meta = document.querySelectorAll('script[type="application/ld+json"]');
-    
-    meta.forEach(element => {
+
+    for (let element of meta) {
+
+        if (articleDate) {
+            break;
+        }
         
         const text = element.innerHTML;
         const metaArray = JSON.parse(text);
 
-        if (Array.isArray(metaArray)) {
-            console.log('ggggg >>>>>>>>>> ');
-            metaArray.forEach(i => {
-                // console.log('got array line ', i); 
+        if (Array.isArray(metaArray)) {            
+            for (let i of metaArray) {                
                 console.log('got pub date!!! ', i.datePublished);
-                 if (i.datePublished) {
-                     //todo break out of for loop
-                    articleDate = i.datePublished;
-                    return articleDate;
-                 }                
-            } );           
-        }        
-    });
-    
+                if (i.datePublished) {                    
+                    articleDate = new Date(i.datePublished);
+                    break;
+                }
+            };
+        }
+    }
+    return articleDate;
+};
+
+
     // const metaDate = document.querySelector('meta[property="article:published_time"]');
-    
+
     // if (metaDate && metaDate.content) {
     //     return new Date(metaDate.content);
     // }
-}
 
 module.exports = {
-    isRecentArticle: (document) => {     
+    isRecentArticle: (document) => {
 
         const articleDate = findArticleDate(document);
-        
-        if (!articleDate) {            
+
+        if (!articleDate) {
             return false;
         } else {
-            
-            const today = new Date();            
+
+            const today = new Date();
             const timeDif = today.getTime() - articleDate.getTime();
-            const dayDif = timeDif / (1000 * 3600 * 24);                                    
+            const dayDif = timeDif / (1000 * 3600 * 24);
 
             logger.debug('article ' + document.title + ' is ' + dayDif + ' days old');
 
